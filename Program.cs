d52fd7aff8ac8dbf7fcbfe527bbd74265a7ecb46
gitwebserver <48014431+gitwebserver@users.noreply.github.com>
2023-12-06 15:28:14 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-gkerjoighjrgoi
+﻿using System;
+
+namespace mots_glisses
+{
+    class Program
+    {
+
+    }
+}
